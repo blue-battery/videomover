40184c6a91d0c86a5d344cd468402a4718e3e3ae
jingpengwu <jingpeng.wu@gmail.com>
2017-05-25 23:35:28 -0400

    working code, tested. add auto deleting of the zoom video

--- videomover/download_all.py
+++ videomover/download_all.py
@@ -15,12 +15,20 @@ def download_zoom_video( output_dir='/tmp' ):
         recording_list = client.recording.list( host_id = user_id )
         meeting_records = recording_list.json()['meetings']
         for meeting_record in meeting_records:
+            print(meeting_record)
             for recording_file in meeting_record['recording_files']:
-                file_name = '{}/{}.mp4'.format(output_dir, recording_file['id'])
+                file_name = '{}/{}_{}.mp4'.format(output_dir,
+                        recording_file['recording_start'],
+                        recording_file['meeting_id'])
                 url = recording_file['download_url']
                 print(url)
                 urllib.request.urlretrieve(url, file_name)
-                os.system("python ./upload_video.py --file {} --title {}".format(file_name, file_name))
+                os.system("python ./upload_video.py --file {} --title {}".format(
+                    file_name, os.path.basename(file_name)))
+                # delete uploaded video in Zoom cloud
+                client.recording.delete(
+                        meeting_id  = recording_file['meeting_id'],
+                        file_id     = recording_file['id'])
 
 if __name__ == '__main__':
     import argparse
